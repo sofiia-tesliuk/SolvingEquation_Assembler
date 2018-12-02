f86db34d6f3882d9b7a51a0684dabe57311494fa
sophia-tslk <teslyuk.sofia@gmail.com>
2018-12-02 20:26:34 +0200

    finally dividing;

--- AssemblerMacOS/main.c
+++ AssemblerMacOS/main.c
@@ -3,6 +3,7 @@
 
 
 void solving_equation(int* a, int* b, int* c);
+int test_op(int a, int b);
 
 
 int main() {
@@ -19,14 +20,18 @@ int main() {
    b = (int *) malloc(4*sizeof(int));
 
    for (int i = 0; i < 4; i++){
-     a[i] = 3;
-     b[i] = 2;
+     a[i] = 18;
+     b[i] = 3;
      c[i] = 0;
    }
    printf("%s\n", "Hello from this point!");
     solving_equation(a, b, c);
     for (int i = 0; i < 4; i++){
-        printf("%d + %d = %d\n", a[i], b[i], c[i]);
+        printf("%d / %d = %d\n", a[i], b[i], c[i]);
     }
+
+    int f = 5;
+    int g = 5;
+    printf("%s\n%d + %d = %d\n", "Just adding", f, g, test_op(f, g));
     return 0;
 }
--- AssemblerMacOS/solving_equation.asm
+++ AssemblerMacOS/solving_equation.asm
@@ -2,14 +2,22 @@ format ELF64
 
 section '.text' executable
 public solving_equation as 'solving_equation'
+public test_op as 'test_op'
 public start
 
 
+test_op:
+    add esi, edi
+    mov edi, eax
+    ret
 
 solving_equation:
-    add rdi, rsi
-    mov rdx, rdi
-    ret
+    mov r8, rdx
+    mov rdx, 0
+    mov rax, [rdi]
+    mov rcx, [rsi]
+    div rcx
+    mov [r8], rax
 
 start:
 
